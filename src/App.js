90cb3f4cb07bb9c3ac9063206d2c9517179eedc6
Viktor Lukashov <viktor@vaadin.com>
2018-09-12 16:52:59 +0300

    configure relay environment and check it works
    based on the Relay Modern Quick Start Guide https://facebook.github.io/relay/docs/en/quick-start-guide.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
+import {graphql, QueryRenderer} from 'react-relay';
 import logo from './logo.svg';
 import './App.css';
+
+import environment from './relay-env';
+
+class RelayApp extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query App_UserQuery {
+            viewer {
+              id
+            }
+          }
+        `}
+        variables={{}}
+        render={({error, props}) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          return <div>User ID: {props.viewer.id}</div>;
+        }}
+      />
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -10,9 +40,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RelayApp></RelayApp>
       </div>
     );
   }
---
+++ src/relay-env.js
@@ -0,0 +1,29 @@
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} from 'relay-runtime';
+
+function fetchQuery(
+  operation,
+  variables,
+) {
+  const url = new URL("http://localhost:3030/graphql");
+  const params = {
+    query: operation.text,
+    variables: JSON.stringify(variables),
+  };
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+  return fetch(url).then(response => {
+    return response.json();
+  });
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
